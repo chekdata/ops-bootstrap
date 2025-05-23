2222a1d55eb937ce355742b512a2111491a4ef8e
赵建新 <13079972+zhao-jianxin2023@user.noreply.gitee.com>
2025-05-23 15:18:52 +0800

    上传行程时间阈值调整5min->4min

--- common_task/tasks.py
+++ common_task/tasks.py
@@ -1306,12 +1306,12 @@ async def handle_merge_task(_id, trip_id, is_last_chunk=False, is_timeout=False)
                     else:
                         logger.error(f"进程池处理数据失败，用户: {user.name}, 错误信息: {message}")
                 else:
-                    logger.error(f"用户 {_id} 不存在, 或行程持续时间小于300s. 无法处理行程数据 {csv_path_list}.")
+                    logger.error(f"用户 {_id} 不存在, 或行程持续时间小于240s. 无法处理行程数据 {csv_path_list}.")
 
                 if not is_valid_interval:    
-                    logger.warning(f"CSV文件 {csv_path_list} 时间间隔小于300秒，跳过处理")
+                    logger.warning(f"CSV文件 {csv_path_list} 时间间隔小于240秒，跳过处理")
                 else:
-                    logger.info(f"CSV文件 {csv_path_list} 时间间隔大于300秒，正常处理")
+                    logger.info(f"CSV文件 {csv_path_list} 时间间隔大于240秒，正常处理")
 
             if csv_path_list and isinstance(csv_path_list, list) and len(csv_path_list) > 0:
                 for csv_path in csv_path_list:
@@ -1858,6 +1858,7 @@ def get_csv_time_interval(csv_path_list):
     获取CSV文件时间间隔
     """
     try:
+        time_interval_thre = 240   # csv时间间隔4分钟,低于该阈值不做行程不做上传小程序处理
         if not (csv_path_list and isinstance(csv_path_list, list) and len(csv_path_list) > 0):
             logger.error(f"CSV文件列表为空!")
             return False
@@ -1878,11 +1879,11 @@ def get_csv_time_interval(csv_path_list):
             else:
                 logger.error(f"CSV文件{csv_path}中没有time列")
                 continue
-        if total_time > 300:
-            logger.info(f"CSV文件 {csv_path} 时间间隔大于300秒，正常处理")
+        if total_time > time_interval_thre:
+            logger.info(f"CSV文件 {csv_path} 时间间隔大于{time_interval_thre}秒，正常处理")
             return True
         else:
-            logger.warning(f"CSV文件 {csv_path} 时间间隔小于300秒，不做处理")
+            logger.warning(f"CSV文件 {csv_path} 时间间隔小于{time_interval_thre}秒，不做处理")
             return False
         
     except Exception as e:
