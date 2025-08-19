a4b1fd2b22ed885fcb90eaab713b6bc629b48ac1
key <14458591+key_pan@user.noreply.gitee.com>
2025-08-19 06:48:12 +0000

    update common_task/tasks.py.
    Signed-off-by: key <14458591+key_pan@user.noreply.gitee.com>

--- common_task/tasks.py
+++ common_task/tasks.py
@@ -2169,7 +2169,7 @@ def handle_message_data(total_message,trip_id,model,hardware_version,software_ve
                     base_url = settings.GENERATE_JOURNEY_REPORT
                     )
 
-                    if result:
+                    if result and core_Journey_profile.journey_status in ['正常','行程上传中','行程创建中']:
         
                         core_Journey_profile.longimg_file_path = result.get('data',{}).get('url')
                         core_Journey_profile.save()
